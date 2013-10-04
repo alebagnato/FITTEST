9fcb3ce62ef51210f16be06f8d1556c56199afec
rtiella@gmail.com <rtiella@gmail.com>
2013-10-04 13:14:27 +0000

    - efsm2mon moved aside transformtools

---
+++ sub-projects/efsm2mon/src/main/java/eu/fittest/fbk/efsm2ct/efsm2mon/tool/generator/velocity/Utils.java
@@ -0,0 +1,24 @@
+package eu.fittest.fbk.efsm2ct.efsm2mon.tool.generator.velocity;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+/**
+ * 
+ * @author tiella
+ */
+public class Utils {
+
+	static void showClasspath() {
+		// Get the System Classloader
+		ClassLoader sysClassLoader = ClassLoader.getSystemClassLoader();
+
+		// Get the URLs
+		URL[] urls = ((URLClassLoader) sysClassLoader).getURLs();
+
+		for (int i = 0; i < urls.length; i++) {
+			System.out.println(urls[i].getFile());
+		}
+	}
+
+}
---
+++ sub-projects/efsm2mon/src/main/java/eu/fittest/fbk/efsm2ct/efsm2mon/tool/model/Tool.java
@@ -0,0 +1,17 @@
+package eu.fittest.fbk.efsm2ct.efsm2mon.tool.model;
+
+/**
+ * 
+ * @author tiella
+ */
+public class Tool {
+
+	public String getName() {
+		return "eu.fittest.fbk.efsm2ct.efsm2mon";
+	}
+
+	public String getVersion() {
+		return "1.0.0";
+	}
+
+}
