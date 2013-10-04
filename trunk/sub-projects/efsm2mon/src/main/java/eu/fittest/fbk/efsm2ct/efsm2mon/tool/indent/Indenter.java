03ba8b92c473fb722164b3e11394ba4b08190098
rtiella@gmail.com <rtiella@gmail.com@ac1ce02c-73a6-9d54-f306-3bdc1d0c215f>
2013-10-04 13:14:27 +0000

    - efsm2mon moved aside transformtools

---
+++ trunk/sub-projects/efsm2mon/src/main/java/eu/fittest/fbk/efsm2ct/efsm2mon/tool/indent/Indenter.java
@@ -0,0 +1,9 @@
+package eu.fittest.fbk.efsm2ct.efsm2mon.tool.indent;
+
+/**
+ * 
+ * @author tiella
+ */
+public interface Indenter {
+	String indent(String rawOut);
+}
---
+++ trunk/sub-projects/efsm2mon/src/main/java/eu/fittest/fbk/efsm2ct/efsm2mon/tool/model/Transition.java
@@ -0,0 +1,43 @@
+package eu.fittest.fbk.efsm2ct.efsm2mon.tool.model;
+
+/**
+ * 
+ * @author tiella
+ */
+public class Transition {
+
+	private State source;
+	private State target;
+	private Mutator mutator;
+
+	public State getSource() {
+		return source;
+	}
+
+	public void setSource(State source) {
+		this.source = source;
+		source.addOutgoing(this);
+	}
+
+	public State getTarget() {
+		return target;
+	}
+
+	public void setTarget(State target) {
+		this.target = target;
+		target.addIncoming(this);
+	}
+
+	public String toString() {
+		return source + "->" + target;
+	}
+
+	public Mutator getMutator() {
+		return mutator;
+	}
+
+	public void setMutator(Mutator mutator) {
+		this.mutator = mutator;
+	}
+
+}
