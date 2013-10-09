90ca6bbb3d1dccb8e6a925b1cb4096f6a5176991
rtiella@gmail.com <rtiella@gmail.com@ac1ce02c-73a6-9d54-f306-3bdc1d0c215f>
2013-10-09 11:51:01 +0000

    - headers with license statement

--- trunk/sub-projects/transformtools/eu.fittest.fbkflex/src/main/java/eu/fittest/flexdriver/FlexObjectDriver.java
+++ trunk/sub-projects/transformtools/eu.fittest.fbkflex/src/main/java/eu/fittest/flexdriver/FlexObjectDriver.java
@@ -1,27 +1,15 @@
-/*	
- *	License
- *	
- *	This file is part of The SeleniumFlex-API.
- *	
- *	The SeleniumFlex-API is free software: you can redistribute it and/or
- *  modify it  under  the  terms  of  the  GNU  General Public License as 
- *  published  by  the  Free  Software Foundation,  either  version  3 of 
- *  the License, or any later version.
- *
- *  The SeleniumFlex-API is distributed in the hope that it will be useful,
- *  but  WITHOUT  ANY  WARRANTY;  without  even the  implied  warranty  of
- *  MERCHANTABILITY   or   FITNESS   FOR  A  PARTICULAR  PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with The SeleniumFlex-API.
- *	If not, see http://www.gnu.org/licenses/
- *
- *
- *	Modified for FITTEST project by CuND/FBK
- *	Date: May 2011
- *
- */
+/*
+
+Copyright (c) 2013, FBK - Fondazione Bruno Kessler http://www.fbk.eu
+All rights reserved. 
+
+This program and the accompanying materials are made available under the terms of
+the 3-Clause BSD License which accompanies this distribution, and is available at
+http://www.opensource.org/licenses/BSD-3-Clause. The research leading to these
+results has received funding from the European Community`s Seventh Framework
+Programme (FP7/2007-2013) under the grant agreement FP7-257574 FITTEST.
+
+*/
  
  /*   Contributed by Black Pepper Software Ltd.  */
  
